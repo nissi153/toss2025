4f50b16d862ff423bf46e4ad306d85339b0558e9
nissi153 <nissisoft21@gmail.com>
2025-02-04 11:08:09 +0900

    업데이트

--- react/my-app/src/App1.js
+++ react/my-app/src/App1.js
@@ -1,7 +1,21 @@
-import E1, { E2, E3, E4 } from "./1-element/Element";
+import * as E from "./1-element/Element";
+import * as ex from "./1-element/Ex";
 
 function App() {
-  return E4;
+  //이 방식은 JSX로 렌더링하지 않고
+  // React 컴퍼넌트 자체 또는 JSX 요소로 작성된 변수를 반환하는 경우.
+  // React.createElement(E.E6)로 실행한다.
+  //return E.E6;
+
+  //이 방식은 React의 함수형(클래스형) 컴퍼넌트를
+  // JSX 문법으로 호출하여 반환하는 경우.
+  // return <E.Hello name="홍길동" />;
+  // return <E.ConfirmDialog />;
+
+  //연습문제
+  // return <ex.Namecard />;
+  // return <ex.Greeting name="홍길동" age="30" />;
+  return <ex.ProductList />;
 }
 
 export default App;
