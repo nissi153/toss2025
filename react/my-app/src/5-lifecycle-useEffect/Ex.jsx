1729ef4d65a1f21eeabf13c2a614fd46ede2b75a
nissi153 <nissisoft21@gmail.com>
2025-02-05 13:33:22 +0900

    Create Ex.jsx

---
+++ react/my-app/src/5-lifecycle-useEffect/Ex.jsx
@@ -0,0 +1,83 @@
+// Ex.jsx
+// 연습문제1: 컴포넌트 마운트 시 콘솔에 메시지 출력하기
+// 목표: 컴포넌트가 마운트될 때 콘솔에 "컴포넌트가 마운트되었습니다." 메시지를 출력하세요.
+// 요구사항:
+// useEffect를 사용하여 컴포넌트가 마운트될 때 한 번만 실행되도록 설정하세요.
+// 힌트: 빈 배열 []을 의존성 배열로 사용하세요.
+import React, { useState, useEffect } from "react";
+
+export function MountMessage() {
+  useEffect(() => {
+    console.log("컴포넌트가 마운트되었습니다");
+  }, []); // 빈 배열: 마운트 시 한 번만 실행
+
+  return (
+    <div>
+      <h1>리액트 컴포넌트 마운트 메시지</h1>
+    </div>
+  );
+}
+
+// 연습문제2. 상태값이 변경될 때 메시지 출력하기
+// 목표: 버튼을 클릭하여 상태값을 변경할 때마다 콘솔에 상태값이 출력되도록 만드세요.
+// 요구사항:
+// useEffect를 사용하여 상태값이 변경될 때마다 메시지를 출력하세요.
+// 힌트: useEffect의 의존성 배열에 상태값을 넣으세요.
+export function UpdateMessage() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`현재 카운트 값: ${count}`);
+  }, [count]); // count가 변경될 때마다 실행
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </div>
+  );
+}
+
+//연습문제3: 타이머 기능 (마운트 및 언마운트)
+// 목표: 컴포넌트가 마운트되면 1초마다 상태값이 증가하는 타이머를 시작하고, 컴포넌트가 언마운트될 때 타이머를 정리하세요.
+// 요구사항:
+// useEffect를 사용하여 마운트 시 타이머를 시작하고, 언마운트 시 타이머를 정리하세요.
+// 1초마다 상태값을 증가시키세요.
+// 힌트: setInterval과 clearInterval 사용
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // setSeconds(seconds + 1);
+
+      // prev는 이전 상태 값을 의미합니다.
+      // 왜 prev가 필요한가?
+      // prev를 사용하면 React가 보장하는 최신 상태 값에 기반해
+      // 안전하게 상태를 업데이트할 수 있습니다.
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    // 언마운트 시 타이머 정리
+    return () => clearInterval(timer);
+  }, []);
+
+  return <h1>타이머: {seconds}초</h1>;
+}
+
+// 4. 윈도우 크기 변경 감지
+// 목표: 윈도우의 크기가 변경될 때마다 현재 창의 너비를 화면에 표시하세요.
+// 요구사항:
+// useEffect로 이벤트 리스너를 등록하고 창 크기가 변경될 때마다 상태를 업데이트하세요.
+// 언마운트 시 이벤트 리스너를 해제하세요.
+// 힌트: window.addEventListener와 window.removeEventListener
+
+//연습문제5: API 호출 및 데이터 로드
+// 목표: 컴포넌트가 마운트될 때 API 호출을 통해 데이터를 가져와 화면에 표시하세요.
+// 요구사항:
+// 1. URL: https://jsonplaceholder.typicode.com/posts
+// 2. 상위 10개의 포스트 테이터만 출력하세요.
+// 3. useEffect를 사용하여 컴포넌트가 마운트될 때 데이터를 로드하세요.
+// 4. 데이터를 로드한 후 상태에 저장하고 화면에 출력하세요.
+// 힌트: fetch 또는 axios 모듈 사용 가능합니다.
