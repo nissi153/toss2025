1eeed719060a7c8837ba3b3b738a1b99545cff0d
nissi153 <nissisoft21@gmail.com>
2025-02-06 15:43:05 +0900

    update

--- react/my-app/src/App8.js
+++ react/my-app/src/App8.js
@@ -1,9 +1,9 @@
 // App8.js
 import React from "react";
-import * as E from "./8-event/Event";
+import * as E from "./8-event/Ex";
 
 function App() {
-  return <E.Event2 />;
+  return <E.Event3 />;
 }
 
 export default App;
