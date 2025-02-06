f524979d0ace5e1fcd29a902156a66d90b3a8915
nissi153 <nissisoft21@gmail.com>
2025-02-06 14:21:21 +0900

    update

--- react/my-app/src/App7.js
+++ react/my-app/src/App7.js
@@ -1,9 +1,10 @@
 // App7.js
 import React from "react";
-import * as E from "./7-hook/Hook";
+// import * as E from "./7-hook/Hook";
+import Counter from "./7-hook/CustomHook";
 
 function App() {
-  return <E.Counter7 />;
+  return <Counter />;
 }
 
 export default App;
---
+++ react/my-app/src/App8.js
@@ -0,0 +1,9 @@
+// App8.js
+import React from "react";
+import * as E from "./8-event/Event";
+
+function App() {
+  return <E.Event2 />;
+}
+
+export default App;
