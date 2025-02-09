0a152ee0ef05d72ef375abbf9231b19dd9985ba8
nissi153 <nissisoft21@gmail.com>
2025-02-10 08:20:51 +0900

    update

--- react/my-app/src/1-element/Element.jsx
+++ react/my-app/src/1-element/Element.jsx
@@ -14,6 +14,7 @@
 
 import React from "react";
 //React 17이상에서는 import하지 않아도 JSX 사용 가능함.
+import "./element.css";
 
 const E1 = <h1 className="greeting">리액트 엘리먼트1입니다.</h1>;
 export const E2 = React.createElement(
--- react/my-app/src/App1.js
+++ react/my-app/src/App1.js
@@ -1,11 +1,11 @@
 import * as E from "./1-element/Element";
-import * as ex from "./1-element/Ex";
+// import * as ex from "./1-element/Ex";
 
 function App() {
   //이 방식은 JSX로 렌더링하지 않고
   // React 컴퍼넌트 자체 또는 JSX 요소로 작성된 변수를 반환하는 경우.
   // React.createElement(E.E6)로 실행한다.
-  //return E.E6;
+  return E.E6;
 
   //이 방식은 React의 함수형(클래스형) 컴퍼넌트를
   // JSX 문법으로 호출하여 반환하는 경우.
@@ -15,7 +15,7 @@ function App() {
   //연습문제
   // return <ex.Namecard />;
   // return <ex.Greeting name="홍길동" age="30" />;
-  return <ex.ProductList />;
+  // return <ex.ProductList />;
 }
 
 export default App;
