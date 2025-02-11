9f5df17fc4b8ec65f20b69fd73b4a1511f96bee3
nissi153 <nissisoft21@gmail.com>
2025-02-11 10:53:11 +0900

    update

--- react/my-app/src/App13.js
+++ react/my-app/src/App13.js
@@ -1,9 +1,9 @@
 // App13.js
 import React from "react";
-import StyledComponent1 from "./13-style/StyledComponent1";
+import StyledComponent3 from "./13-style/StyledComponent3";
 
 function App() {
-  return <StyledComponent1 />;
+  return <StyledComponent3 />;
 }
 
 export default App;
