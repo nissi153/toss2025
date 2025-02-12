b7b37648a28e8b19ebe771348e2a134e94b98f76
nissi153 <nissisoft21@gmail.com>
2025-02-12 13:36:47 +0900

    update

---
+++ react/my-app/src/14-context/ThemeContextProvider.jsx
@@ -0,0 +1,24 @@
+import { useState, useCallback } from "react";
+import ThemeContext from "./ThemeContext";
+
+function ThemeContextProvider() {
+  const [theme, setTheme] = useState("light");
+
+  //useMemo : 값이 저장해 놓음.
+  //useCallback : 함수을 저장해 놓음.
+  const toggleTheme = useCallback(() => {
+    if (theme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ThemeContextUseContext />
+    </ThemeContext.Provider>
+  );
+}
+
+export default ThemeContextProvider;
