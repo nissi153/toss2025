1dcca55072d1ffdde3aa89f2db7fc20346bc18b1
nissi153 <nissisoft21@gmail.com>
2025-02-19 10:41:26 +0900

    Update ex13-parameter.js

--- nodejs/basics/ex13-parameter.js
+++ nodejs/basics/ex13-parameter.js
@@ -6,17 +6,35 @@ const app = express()
 const port = 3000
 
 //인덱스 페이지
-app.get('/', () => {})
+app.get('/', (req, res) => {
+  res.status(200)
+  res.send('Hello Express~')
+})
 //게시글 목록 조회
-app.get('/posts', () => {})
+app.get('/posts', (req, res) => {
+  res.status(200)
+  res.send('게시글 목록 조회')
+})
 //게시글 단건 조회
-app.get('/posts/:id', () => {})
+app.get('/posts/:id', (req, res) => {
+  res.status(200)
+  res.send(`게시글 단건 조회 ID:${req.params.id}`)
+})
 //게시글 추가
-app.post('/posts', () => {})
+app.post('/posts', (req, res) => {
+  res.status(200)
+  res.send('게시글 추가')
+})
 //게시글 수정
-app.put('/posts/:id', () => {})
+app.put('/posts/:id', (req, res) => {
+  res.status(200)
+  res.send(`게시글 수정 ID:${req.params.id}`)
+})
 //게시글 삭제
-app.delete('/posts/:id', () => {})
+app.delete('/posts/:id', (req, res) => {
+  res.status(200)
+  res.send(`게시글 삭제 ID:${req.params.id}`)
+})
 
 app.listen(port, () => {
   console.log(`${port}번 포트에서 서버 실행 중...`)
