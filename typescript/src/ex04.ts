b766c66e07bd6f0c3b1dc8257eab39645836ea05
nissi153 <nissisoft21@gmail.com>
2025-02-25 09:17:53 +0900

    Update ex04.ts

--- typescript/src/ex04.ts
+++ typescript/src/ex04.ts
@@ -2,11 +2,11 @@
 
 //타입 별칭(Type Alias)
 type User = {
-  id: number,
-  name: string,
-  nickname: string,
-  birth: string,
-  location: string,
+  id: number
+  name: string
+  nickname: string
+  birth: string
+  location: string
   phone: string
 }
 
@@ -16,7 +16,7 @@ let user1: User = {
   nickname: 'hong',
   birth: '2000.01.01',
   location: '한양',
-  phone: '010-1111-2222'
+  phone: '010-1111-2222',
 }
 
 let user2: User = {
@@ -25,5 +25,35 @@ let user2: User = {
   nickname: 'dang',
   birth: '2000.01.01',
   location: '한양',
-  phone: '010-1111-2222'
+  phone: '010-1111-2222',
+}
+
+//객체 리터럴
+type CountryCodes = {
+  Korea: string
+  UnitedState: string
+  UnitedKingdom: string
+}
+let countryCodes: CountryCodes = {
+  Korea: 'ko',
+  UnitedState: 'us',
+  UnitedKingdom: 'uk',
+  //200개라면?
+}
+// 인덱스 시그니쳐
+type CountryCodes2 = {
+  [key: string]: string
+}
+let countryCodes2: CountryCodes2 = {
+  Korea: 'ko',
+  UnitedState: 'us',
+  UnitedKingdom: 'uk',
+  Japan: 123,
+}
+type CountryCodes3 = {
+  [key: string]: string
+  Korea: 'ko' //Korea는 꼭 있어야 됨.
+}
+let countryCodes3: CountryCodes3 = {
+  Korea: 'ko',
 }
