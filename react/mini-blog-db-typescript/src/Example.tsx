e834a44b0fbda2b28b7aff5cebbfcf5c976e81c3
nissi153 <nissisoft21@gmail.com>
2025-02-26 09:48:03 +0900

    Update Example.tsx

--- react/mini-blog-db-typescript/src/Example.tsx
+++ react/mini-blog-db-typescript/src/Example.tsx
@@ -1,4 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+interface MyButtonProps {
+  primary?: boolean;
+}
+
+const MyButton = styled.button<MyButtonProps>`
+  background: ${(props) => (props.primary ? "blue" : "gray")};
+  color: white;
+  padding: 10px;
+`;
+
+export function CustomButton() {
+  return <MyButton primary={true}>Click me</MyButton>;
+}
 
 //리액트에서 함수형 컴퍼넌트의 타입 정의(생략 가능)
 //React.FC
@@ -6,8 +21,6 @@ interface ButtonProps {
   label: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label }: ButtonProps) => {
+export const Button: React.FC<ButtonProps> = ({ label }: ButtonProps) => {
   return <button>{label}</button>;
 };
-
-export default Button;
