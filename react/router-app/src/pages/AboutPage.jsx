f852c4b885c73efd618379e83ec84e94e529d60d
nissi153 <nissisoft21@gmail.com>
2025-03-04 08:30:06 +0900

    Update AboutPage.jsx

--- react/router-app/src/pages/AboutPage.jsx
+++ react/router-app/src/pages/AboutPage.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function AboutPage() {
+  const navigate = useNavigate();
+
+  const gotoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
       <h1>소개 페이지</h1>
       <p>이곳은 소개 페이지입니다.</p>
+      <button onClick={gotoBack}>뒤로 이동</button>
     </div>
   );
 }
