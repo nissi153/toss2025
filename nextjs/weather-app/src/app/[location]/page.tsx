27abd086631252f7b6551b9f1236e61631a90cc7
nissi153 <nissisoft21@gmail.com>
2025-03-27 11:39:54 +0900

    update

--- nextjs/weather-app/src/app/[location]/page.tsx
+++ nextjs/weather-app/src/app/[location]/page.tsx
@@ -1,23 +1,22 @@
-// localhost:3000/[location]
-// localhost:3000/seoul
-// localhost:3000/NYC
-// localhost:3000/london
+'use client'
+
+import HomeButton from '../components/HomeButton'
+
 type Props = {
   params: {
     location: string
   }
-  // params: Promise<{ location: string }>;
 }
-// next.js 13버전에서 async를 통해서 params 값을 얻어온다.
-export default async function Detail({ params }: Props) {
-  const { location } = await params
-  console.log(location)
 
+export default function Detail({ params }: Props) {
+  const { location } = params
   const name = location === 'seoul' ? '서울' : location
 
   return (
     <>
       <h1>{name}의 3일치 날씨 예보</h1>
+      <br />
+      <HomeButton />
     </>
   )
 }
--- nextjs/weather-app/src/app/page.tsx
+++ nextjs/weather-app/src/app/page.tsx
@@ -1,9 +1,26 @@
-import styles from "./page.module.css";
+import styles from './page.module.css'
+import Link from 'next/link'
+
+const getCurrentWeather = async () => {
+  const res = await fetch('')
+  return res.json()
+}
 
 export default function Home() {
   return (
     <>
       <h1>Main Page</h1>
+      <ul>
+        <li>
+          <Link href="/seoul">서울</Link>
+        </li>
+        <li>
+          <Link href="/NYC">뉴욕</Link>
+        </li>
+        <li>
+          <Link href="/london">런던</Link>
+        </li>
+      </ul>
     </>
-  );
+  )
 }
