aa979d11500dcf8d46f2eb3a695dc2397cdb74c0
nissi153 <nissisoft21@gmail.com>
2025-03-28 13:24:55 +0900

    update

---
+++ java/javaExamples/src/ex03.java
@@ -0,0 +1,10 @@
+public class ex03 {
+    public static void main(String[] args) {
+        //데이터 타입(자료형)
+        //기본자료형 8개
+        //정수형 : int(4) long(8) short(2) byte(1)
+        //실수형 : float(4) double(8)
+        //논리형 : boolean(1)
+        //문자형(내부적으로 숫자형) : char(2)
+    }
+}
