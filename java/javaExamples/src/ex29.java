3a4f917dd1f484f4e3cc79c21c07705be9dd4502
nissi153 <nissisoft21@gmail.com>
2025-03-31 16:45:11 +0900

    update

---
+++ java/javaExamples/src/ex29.java
@@ -0,0 +1,17 @@
+//연습문제 - 클래스 상속
+//
+//부모클래스 - 나무     Wood
+//          속성 가격 int price = 1000
+//         행동 불탄다 burn()
+//자식클래스 - 나무책상 WoodDesk
+//          속성  color  "흰색"
+//          행동  study   "공부를 한다."
+//         - 나무의자 WoodChair
+//          속성  color  "갈색"
+//           행동  sit    "앉는다"
+//자식클래스의 객체를 생성하고 속성과 행동을 출력하시오.
+public class ex29 {
+    public static void main(String[] args) {
+
+    }
+}
