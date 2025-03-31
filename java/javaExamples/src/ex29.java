7ec824647a03d5970b45a060b884e6388e47e9f9
nissi153 <nissisoft21@gmail.com>
2025-03-31 17:19:13 +0900

    Update ex29.java

--- java/javaExamples/src/ex29.java
+++ java/javaExamples/src/ex29.java
@@ -10,8 +10,41 @@
 //          속성  color  "갈색"
 //           행동  sit    "앉는다"
 //자식클래스의 객체를 생성하고 속성과 행동을 출력하시오.
+class Wood {
+    int price = 1000;
+
+    void burn() {
+        System.out.println("불에 잘 탄다.");
+    }
+}
+
+class WoodDesk extends Wood {
+    String color = "흰색";
+
+    void study() {
+        System.out.println("공부한다.");
+    }
+}
+
+class WoodChair extends Wood {
+    String color = "갈색";
+
+    void sit() {
+        System.out.println("앉는다.");
+    }
+}
+
 public class ex29 {
     public static void main(String[] args) {
-
+        WoodDesk desk = new WoodDesk();
+        System.out.println(desk.price);
+        System.out.println(desk.color);
+        desk.burn();
+        desk.study();
+        WoodChair chair = new WoodChair();
+        System.out.println(chair.price);
+        System.out.println(chair.color);
+        chair.burn();
+        chair.sit();
     }
 }
