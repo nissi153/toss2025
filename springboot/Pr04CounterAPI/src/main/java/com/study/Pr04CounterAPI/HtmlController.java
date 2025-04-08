b066ad9eaf864915b330773f61da2537acc5c75f
nissi153 <nissisoft21@gmail.com>
2025-04-08 17:18:44 +0900

    update

--- springboot/Pr04CounterAPI/src/main/java/com/study/Pr04CounterAPI/HtmlController.java
+++ springboot/Pr04CounterAPI/src/main/java/com/study/Pr04CounterAPI/HtmlController.java
@@ -1,12 +1,18 @@
 package com.study.Pr04CounterAPI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HtmlController {
+    @Autowired
+    private Counter counter;
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model){
+        model.addAttribute("count", counter.getCount());
         return "index";
     }
 }
