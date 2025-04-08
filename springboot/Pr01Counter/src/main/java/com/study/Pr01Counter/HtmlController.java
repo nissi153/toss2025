e4b27363de850ce266a6cb3601b008417a5eb4a1
nissi153 <nissisoft21@gmail.com>
2025-04-08 16:19:34 +0900

    Update HtmlController.java

--- springboot/Pr01Counter/src/main/java/com/study/Pr01Counter/HtmlController.java
+++ springboot/Pr01Counter/src/main/java/com/study/Pr01Counter/HtmlController.java
@@ -2,6 +2,7 @@ package com.study.Pr01Counter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,9 +12,20 @@ public class HtmlController {
     private Counter counter;
 
     @GetMapping("/")
-    public String main(){
-        System.out.println("counter.getCount() = " + counter.getCount());
-        
+    public String main(Model model){
+        model.addAttribute("count", counter.getCount());
         return "index"; //index.html이 응답결과로 반환된다.
+    }
+    @GetMapping("/plus")
+    public String plus(Model model){
+        counter.setCount(counter.getCount() + 1);
+        model.addAttribute("count", counter.getCount());
+        return "index";
+    }
+    @GetMapping("/minus")
+    public String minus(Model model){
+        counter.setCount(counter.getCount() - 1);
+        model.addAttribute("count", counter.getCount());
+        return "index";
     }
 }
