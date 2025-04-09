a21d9d0362a604aa5a08e2c8dd428a8d1c3b5e40
nissi153 <nissisoft21@gmail.com>
2025-04-09 15:48:16 +0900

    update

---
+++ springboot/Pr02Calc/src/main/java/com/study/Pr02Calc/HtmlController.java
@@ -0,0 +1,35 @@
+package com.study.Pr02Calc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HtmlController {
+    @GetMapping("/")
+    public String main(){
+        return "index";
+    }
+    @GetMapping("/calc-submit")
+    public String calcSubmit(@RequestParam("num1") String number1,
+                             @RequestParam("num2") String number2,
+                             @RequestParam("calType") String calType,
+                             Model model) {
+        System.out.println("number1 = " + number1 + ", number2 = " + number2 +
+                ", calType = " + calType);
+
+        int result = 0;
+        if( calType.equals("add") ) {
+            int num1 = Integer.parseInt(number1);
+            int num2 = Integer.parseInt(number2);
+            result = num1 + num2;
+        }
+
+        model.addAttribute("num1", number1);
+        model.addAttribute("num2", number2);
+        model.addAttribute( "result", result);
+        return "index";
+    }
+}
