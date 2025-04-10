ae6439df96914c26ea830116851dcc95f1b734f6
nissi153 <nissisoft21@gmail.com>
2025-04-10 13:56:26 +0900

    update

--- springboot/Ex12H2DBTest/src/main/java/com/study/springboot/MainController.java
+++ springboot/Ex12H2DBTest/src/main/java/com/study/springboot/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -15,6 +16,10 @@ public class MainController {
 
     @GetMapping("/")
     public String main(Model model){
+        memberRepository.save(
+                new MemberEntity(null, "hong", "1234", "홍길동", "ROLE_USER",
+                        LocalDate.parse("2025-04-10")));
+
         List<MemberEntity> list = memberRepository.findAll(); //Select * from member;
         model.addAttribute("list", list);
         return "index";
