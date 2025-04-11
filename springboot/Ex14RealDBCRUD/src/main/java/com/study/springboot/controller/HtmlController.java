fd608322103c201d10c093dc3e907ee7ca482f16
nissi153 <nissisoft21@gmail.com>
2025-04-11 15:12:23 +0900

    Update HtmlController.java

--- springboot/Ex14RealDBCRUD/src/main/java/com/study/springboot/controller/HtmlController.java
+++ springboot/Ex14RealDBCRUD/src/main/java/com/study/springboot/controller/HtmlController.java
@@ -4,11 +4,13 @@ import com.study.springboot.dto.MemberSaveDto;
 import com.study.springboot.entity.MemberEntity;
 import com.study.springboot.entity.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -31,10 +33,24 @@ public class HtmlController {
         return "joinForm";
     }
 
+
+
     @PostMapping("/joinAction")
+    @ResponseBody
     public String joinAction(@ModelAttribute MemberSaveDto dto){
         System.out.println("dto = " + dto.getUserId());
-        return "index";
+        try {
+            MemberEntity entity = dto.toSaveEntity();
+            memberRepository.save( entity );
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return "<script>alert('회원가입실패!');history.back();</script>";
+        }
+        System.out.println("회원가입 성공!");
+
+        return "<script>alert('회원가입성공!');location.href='/';</script>";
     }
 
 }
