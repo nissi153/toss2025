ea47986e02e9f666e37c821ba4a5f60320a9dd0e
nissi153 <nissisoft21@gmail.com>
2025-04-15 10:20:49 +0900

    update

--- springboot/Ex17MyBatis/src/main/java/com/study/springboot/dao/IMemberDao.java
+++ springboot/Ex17MyBatis/src/main/java/com/study/springboot/dao/IMemberDao.java
@@ -1,4 +1,12 @@
 package com.study.springboot.dao;
 
+import com.study.springboot.dto.MemberDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+// @Mapper : 인터페이스 DAO와 MyBatis XML과 연결하는 용도
+@Mapper
 public interface IMemberDao {
+    public List<MemberDto> list();
 }
