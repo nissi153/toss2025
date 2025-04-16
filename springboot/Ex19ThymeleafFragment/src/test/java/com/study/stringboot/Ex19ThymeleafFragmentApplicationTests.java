1120f2f82ed776eed0a5ddf0ceb573b175260ad5
nissi153 <nissisoft21@gmail.com>
2025-04-16 14:41:54 +0900

    update

---
+++ springboot/Ex19ThymeleafFragment/src/test/java/com/study/stringboot/Ex19ThymeleafFragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.stringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex19ThymeleafFragmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
