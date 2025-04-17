bbec8a461713c426a78efe24949b384dc674127d
nissi153 <nissisoft21@gmail.com>
2025-04-17 09:52:43 +0900

    update

---
+++ springboot/Ex21TDD/src/test/java/com/study/springboot/LifeCycleTest.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LifeCycleTest {
+    @Test
+    @DisplayName("테스트1")
+    void testMethod1(){
+        System.out.println("테스트1");
+    }
+
+}
