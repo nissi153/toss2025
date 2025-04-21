d52bfd3a0cdad41e2026385319129b63d1bdc38f
nissi153 <nissisoft21@gmail.com>
2025-04-21 15:50:31 +0900

    update

--- springboot/Ex24Board/src/main/java/com/study/springboot/controller/BoardController.java
+++ springboot/Ex24Board/src/main/java/com/study/springboot/controller/BoardController.java
@@ -1,4 +1,32 @@
 package com.study.springboot.controller;
 
+import com.study.springboot.domain.board.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/")
+    public String main(){
+        return "redirect:/board/listForm";
+    }
+    @GetMapping("/listForm")
+    public String listForm(){
+        return "listForm";
+    }
+
+
 }
+
+
+
+
+
+
