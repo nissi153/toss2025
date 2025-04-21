c04bb98c20e71e59cabcea2ba451cf8407070d42
nissi153 <nissisoft21@gmail.com>
2025-04-21 13:34:27 +0900

    upload

--- springboot/Ex23FileUpload/src/main/java/com/study/springboot/HtmlController.java
+++ springboot/Ex23FileUpload/src/main/java/com/study/springboot/HtmlController.java
@@ -1,8 +1,8 @@
 package com.study.springboot;
 
-import ch.qos.logback.core.model.Model;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -33,6 +35,35 @@ public class HtmlController {
                 file.transferTo( newFileName );
             }
         }
+        //http://localhost:8080/upload/255a0168-cf20-4e37-bd10-2e1d03be8400_image.png
         return "upload";
     }
+    @PostMapping("/upload2")
+    public String upload2(@RequestParam MultipartFile[] uploadfile,
+                          Model model) throws IOException {
+        List<FileDto> list = new ArrayList<>();
+        for( MultipartFile file : uploadfile){
+            if(!file.isEmpty()){
+                //FileDto생성
+                FileDto dto = new FileDto(UUID.randomUUID().toString(),
+                        file.getOriginalFilename(),
+                        file.getContentType());
+                list.add( dto );
+
+                //File을 물리적으로 기록하기
+                File newFileName = new File(dto.getUuid() + "_" + dto.getFileName());
+                file.transferTo( newFileName );
+
+                //DB에 파일이름을 기록한다.
+
+            }
+        }
+        model.addAttribute("files", list);
+        return "result";
+    }
+
 }
+
+
+
+
