c6243a96406d160c3472b143bee32a5627a853d6
nissi153 <nissisoft21@gmail.com>
2025-04-22 14:49:56 +0900

    update

--- springboot/Ex24Board/src/main/java/com/study/springboot/controller/ReplyController.java
+++ springboot/Ex24Board/src/main/java/com/study/springboot/controller/ReplyController.java
@@ -1,4 +1,28 @@
 package com.study.springboot.controller;
 
+import com.study.springboot.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/reply")
+@RequiredArgsConstructor
 public class ReplyController {
+    private final ReplyService replyService;
+
+    //  "/reply/deleteAction?replyIdx=4&boardIdx=1"
+    @GetMapping("/deleteAction")
+    @ResponseBody
+    public String deleteAction(@RequestParam Long replyIdx,
+                               @RequestParam Long boardIdx){
+        replyService.delete( replyIdx );
+
+        return "<script>alert('댓글삭제 성공'); location.href='/board/contentForm?boardIdx="+ boardIdx +"';</script>";
+    }
+
+
 }
--- springboot/Ex24Board/src/main/java/com/study/springboot/service/ReplyService.java
+++ springboot/Ex24Board/src/main/java/com/study/springboot/service/ReplyService.java
@@ -24,4 +24,10 @@ public class ReplyService {
         //List<Reply>를 List<DTO>로 변환 : stream()메소드
         return list.stream().map(ReplyResponseDto::new).collect(Collectors.toList());
     }
+    @Transactional
+    public void delete(Long replyIdx){
+        Reply entity = replyRepository.findById(replyIdx)
+                .orElseThrow( () -> new IllegalArgumentException("댓글 인덱스 오류입니다. replyIdx:"+replyIdx) );
+        replyRepository.delete( entity) ;
+    }
 }
