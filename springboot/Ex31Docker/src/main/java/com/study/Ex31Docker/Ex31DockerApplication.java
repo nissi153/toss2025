0542b1f3f4eefa2bf4dcce6532712bfe7e13fe90
nissi153 <nissisoft21@gmail.com>
2025-05-13 16:01:00 +0900

    docker ci/cd

---
+++ springboot/Ex31Docker/src/main/java/com/study/Ex31Docker/Ex31DockerApplication.java
@@ -0,0 +1,13 @@
+package com.study.Ex31Docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex31DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex31DockerApplication.class, args);
+	}
+
+}
---
+++ springboot/Ex31Docker/src/test/java/com/study/Ex31Docker/Ex31DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex31Docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex31DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
